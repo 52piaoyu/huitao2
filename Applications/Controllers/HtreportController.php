2834734641faceb84aea100b9570909b299eb099
Dream <1579775085@qq.com>
2017-06-01 18:12:35 +0800

    渠道报表

--- Applications/Controllers/HtreportController.php
+++ Applications/Controllers/HtreportController.php
@@ -1,38 +1,38 @@
 <?php
 class HtreportController {
-	//入渠道报表 中间表
-	private function addData($data) {
-		$fields  = M('report')->getTableFields();
-		foreach($fields as $k => $v){
-			if($v == 'id') unset($fields[$k]);
-			if($v == 'system') unset($fields[$k]);
+	/**
+	 * [timingTask 定时任务 计算渠道报表数据入库]
+	 */
+	public function timingTask() {
+		$source = M()->query('SELECT source, system FROM ngw_tracking GROUP BY source,system', 'all');
+		$time = date('Y-m-d', strtotime('-1 day'));
+		foreach($source as $v) {
+			$this->addData($this->calculatio($time, $time, connectionArray(M('tracking')->where("uid is not null AND source = {$v['source']}' and system = {$v['system']}")->select('all'), 'uid')), $v['source'], $v['system']);
 		}
-		$sql = 'REPLACE INTO ngw_report('.('`'.implode('`,`', $fields).'`').')VALUES';
-		foreach($data as $v) {
-			//设置字段默认值为0
-			foreach($fields as $value) if(empty($v[$value])) $v[$value] = 0;
-			$sql .= '('.$v['fee'].','.$v['benifit'].','.$v['td_fee'].','.$v['td_benifit'].','.$v['share'].','.$v['s_percent'].','.$v['keep'].','.$v['track_num'].','.$v['click_num'].','.$v['invatation_num'].',';
-			$sql .= $v['source'] ? "'{$v['source']}'," : "'{$_REQUEST['media_id']}',";
-			$sql .= "'".(date('Y-m-d'))."'";
-			$sql .= '),';
+	}
+	private function addData($data, $source, $system) {
+		$sql = 'INSERT IGNORE INTO ngw_report(fee, benifit, td_fee, td_benifit, share, s_percent, keep, track_num, click_num, invatation_num, source, system, report_date)VALUES';
+		foreach($data as $k => $v) {
+			foreach($v as $key => $value) { if(!$v[$key]) $v[$key] = 0; }	//设置字段默认值
+			$sql .= "({$v['fee']}, {$v['benifit']}, {$v['td_fee']}, {$v['td_benifit']}, {$v['share']}, {$v['s_percent']}, {$v['keep']}, {$v['track_num']}, {$v['click_num']}, {$v['invatation_num']}, '{$source}', '{$system}', '{$v['date']}'),";
 		}
-		return M()->query(rtrim($sql, ','));
+		return M()->exec(rtrim($sql, ','));
 	}
 	public function calculatio($startTime, $endTime, $data) {
 		//生成日期数组
 		$dates = $this->prDates($startTime, $endTime);
-		$HtreportModel = new HtreportModel($data);
+		$HtreportModel = new HtreportModel($data, $startTime, $endTime);
 		$mg_list = array_merge_recursive(
-		    $this->hdArr($HtreportModel->shareRecord($startTime, $endTime)),                    //用户分享记录
-		    $this->hdArr($HtreportModel->shareRate($startTime, $endTime)),                      //用户分享率
-		    $this->hdArr($HtreportModel->channelsNewlyAdded($startTime, $endTime)),             //新增用户
-		    $this->hdArr($HtreportModel->inviteNewlyAdded($startTime, $endTime)),               //邀请新增
-		    $this->hdArr($HtreportModel->validUser($startTime, $endTime)),                      //有效用户
-		    $this->hdArr($HtreportModel->leaveBehindUser($startTime, $endTime)),				//用户留存数
-		    $this->hdArr($HtreportModel->userPurchaseNumber($startTime, $endTime)),             //用户下单额
-		    $this->hdArr($HtreportModel->userFriendProfits($startTime, $endTime)),              //用户带来的毛利润
-		    $this->hdArr($HtreportModel->userMaoProfits($startTime, $endTime)),					//用户的好友带来的毛利润
-		    $this->hdArr($HtreportModel->userFriendPurchaseNumber($startTime, $endTime))        //用户邀请的好友的下单额
+		    $this->hdArr($HtreportModel->shareRecord()),                    //用户分享记录
+		    $this->hdArr($HtreportModel->shareRate()),                      //用户分享率
+		    $this->hdArr($HtreportModel->channelsNewlyAdded()),             //新增用户
+		    $this->hdArr($HtreportModel->inviteNewlyAdded()),               //邀请新增
+		    $this->hdArr($HtreportModel->validUser()),                      //有效用户
+		    $this->hdArr($HtreportModel->leaveBehindUser()),				//用户留存数
+		    $this->hdArr($HtreportModel->userPurchaseNumber()),             //用户下单额
+		    $this->hdArr($HtreportModel->userFriendProfits()),              //用户带来的毛利润
+		    $this->hdArr($HtreportModel->userMaoProfits()),					//用户的好友带来的毛利润
+		    $this->hdArr($HtreportModel->userFriendPurchaseNumber())        //用户邀请的好友的下单额
 		);
 		//字段定义
 		$basic_list = ['fee'=>'','benifit'=>'','td_fee'=>'','td_benifit'=>'','share'=>'','s_percent'=>'','keep'=>'','track_num'=>'','click_num'=>'','invatation_num'=>''];
@@ -46,29 +46,25 @@ class HtreportController {
 	 * [channelOneReport 各渠道折线图]
 	 */
 	public function channelOneReport() {
-		if(!empty($_REQUEST['start_time']) && !empty($_REQUEST['end_time']) && !empty($_REQUEST['media_id'])) {
-        	$startTime = $_REQUEST['start_time'];
-        	//如果截止日期超过当前日期或者大于当前日期则重新计算
-        	if($_REQUEST['end_time'] >= date('Y-m-d')) {
-        		M('report')->where(['report_date' => ['=', date('Y-m-d')], 'source' => ['=', $_REQUEST['media_id']] ])->save();
-        		$endTime = date('Y-m-d');
-        	} else $endTime = $_REQUEST['end_time'];
-        	//查询中间表是否有数据ilo0IL
-        	$intermediateTableData = M('report')->where("report_date BETWEEN '{$startTime}' AND '{$endTime}' AND source = '{$_REQUEST['media_id']}' order by report_date asc")->select('all');
-        	//从中间表里查询出来的数据最后记录的一个日期==当前截止日期  且 开始日期==查出来的第一条记录的日期 则直接return掉
-        	if(!empty($intermediateTableData) && (end($intermediateTableData))['report_date'] == $endTime && $intermediateTableData[0]['report_date'] == $startTime){
-        		foreach($intermediateTableData as &$v) $v['date'] = $v['report_date'];
-        		info('ok', 1, $intermediateTableData);
-        	}
-        	//渠道用户进行计算汇总数据
-        	$data = $this->calculatio($startTime, $endTime, connectionArray(M('tracking')->where("uid is not null AND source = '{$_REQUEST['media_id']}'")->select('all'), 'uid'));
-        	//全部用户进行计算汇总数据
-        	// $data = $this->calculatio($startTime, $endTime, connectionArray(M('uid')->field('objectId')->select('all'), 'objectId'));
-        	//存入中间表
-        	$this->addData($data);
-        	return info('ok', 1, $data);
-		}
-		info('缺少参数', -1);
+		if(empty($_REQUEST['start_time']) || empty($_REQUEST['end_time']) || empty($_REQUEST['media_id'])) info('缺少参数', -1);
+    	$startTime = $_REQUEST['start_time'];
+    	//如果截止日期超过当前日期或者大于当前日期则重新计算
+    	if($_REQUEST['end_time'] >= date('Y-m-d')) {
+    		M('report')->where(['report_date' => ['=', date('Y-m-d')], 'source' => ['=', $_REQUEST['media_id']] ])->save();
+    		$endTime = date('Y-m-d');
+    	} else $endTime = $_REQUEST['end_time'];
+    	/**
+    	 * 先查询中间表是否有数据 如果有则info掉 如果没有则重新计算返回
+    	 * 从中间表里查询出来的数据最后记录的一个日期==当前截止日期  且 开始日期==查出来的第一条记录的日期
+    	 */
+    	$intermediateTableData = M('report')->where("report_date BETWEEN '{$startTime}' AND '{$endTime}' AND source = '{$_REQUEST['media_id']}' order by report_date asc")->select('all');
+    	if(!empty($intermediateTableData) && (end($intermediateTableData))['report_date'] == $endTime && $intermediateTableData[0]['report_date'] == $startTime) {
+    		foreach($intermediateTableData as &$v) $v['date'] = $v['report_date'];
+    		info('ok', 1, $intermediateTableData);
+    	}
+    	$data = $this->calculatio($startTime, $endTime, connectionArray(M('tracking')->where("uid is not null AND source = '{$_REQUEST['media_id']}'")->select('all'), 'uid'));
+    	// $data = $this->calculatio($startTime, $endTime, connectionArray(M('uid')->field('objectId')->select('all'), 'objectId'));
+    	info('ok', 1, $data);
 	}
 	private function prDates($start,$end) {
 		$dt_start = strtotime($start);
@@ -81,9 +77,11 @@ class HtreportController {
 	}
 	private function hdArr($arr) {
 		$_arr = [];
-		foreach($arr as $k => $v) {
-			$_arr[$v['report_date']] = $v;
-			unset($_arr[$v['report_date']]['report_date']);
+		if(!empty($arr)) {
+			foreach($arr as $k => $v) {
+				$_arr[$v['report_date']] = $v;
+				unset($_arr[$v['report_date']]['report_date']);
+			}
 		}
 		return $_arr;
 	}
